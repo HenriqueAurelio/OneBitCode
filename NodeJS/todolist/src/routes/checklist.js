5b28696ba2dce59efd6a1ddf0e979b7431b48999
Henrique <henriqueaureliocsilva@gmail.com>
2022-04-04 10:21:58 -0300

    adding populate

--- NodeJS/todolist/src/routes/checklist.js
+++ NodeJS/todolist/src/routes/checklist.js
@@ -6,7 +6,7 @@ const Checklist = require('../models/checklist')
 
 router.get('/', async (req, res) => {
   try {
-    let checklists = await Checklist.find({})
+    let checklists = await Checklist.find({}).populate('tasks')
     res.status(200).render('checklists/index', { checklists: checklists })
   } catch (error) {
     res.status(500).render('pages/error', { eror: 'Erro ao exibir as Listas' })
@@ -39,7 +39,7 @@ router.post('/', async (req, res) => {
 })
 router.get('/:id', async (req, res) => {
   try {
-    let checklist = await Checklist.findById(req.params.id)
+    let checklist = await Checklist.findById(req.params.id).populate('tasks')
     res.status(200).render('checklists/show', { checklist: checklist })
   } catch (error) {
     res.status(500).render('pages/error', { error: 'Erro ao exibir as Listas' })
